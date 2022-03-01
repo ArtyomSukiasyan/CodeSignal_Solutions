e34af9e5a2f80234a1c7e6dddbc49805d30997f4
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-03-02 00:00:16 +0400

    feat: add 6_makeArrayConsecutive_2.js file

---
+++ intro/6_makeArrayConsecutive_2.js
@@ -0,0 +1,15 @@
+// Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, each statue having an non-negative integer size. Since he likes to make things perfect, he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. He may need some additional statues to be able to accomplish that. Help him figure out the minimum number of additional statues needed.
+
+function solution(statues) {
+  let min = statues[0];
+  let max = statues[0];
+  for (let i = 1; i < statues.length; i++) {
+    if (statues[i] < min) {
+      min = statues[i];
+    }
+    if (statues[i] > max) {
+      max = statues[i];
+    }
+  }
+  return max - min - statues.length + 1;
+}
