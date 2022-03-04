5b266837966168006d38f669d4ad08185a3dc708
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-03-04 08:58:48 +0400

    feat: add 23_boxBlur.js file

---
+++ intro/23_boxBlur.js
@@ -0,0 +1,29 @@
+// Last night you partied a little too hard. Now there's a black and white photo of you that's about to go viral! You can't let this ruin your reputation, so you want to apply the box blur algorithm to the photo to hide its content.
+// The pixels in the input image are represented as integers. The algorithm distorts the input image in the following way: Every pixel x in the output image has a value equal to the average value of the pixel values from the 3 × 3 square that has its center at x, including x itself. All the pixels on the border of x are then removed.
+// Return the blurred image as an integer, with the fractions rounded down.
+
+function solution(image) {
+  let bluredImage = Array.from(
+    Array(image.length - 2),
+    () => new Array(image[0].length - 2)
+  );
+
+  for (let i = 1; i < image.length - 1; i++) {
+    for (let j = 1; j < image[i].length - 1; j++) {
+      bluredImage[i - 1][j - 1] = Math.floor(
+        (image[i - 1][j - 1] +
+          image[i - 1][j] +
+          image[i - 1][j + 1] +
+          image[i][j - 1] +
+          image[i][j] +
+          image[i][j + 1] +
+          image[i + 1][j - 1] +
+          image[i + 1][j] +
+          image[i + 1][j + 1]) /
+          9
+      );
+    }
+  }
+
+  return bluredImage;
+}
